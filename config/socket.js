97ce8bee8f3df8569bf972a50a8f44075a288454
muhammedsouban <muhammedsoubanbi@gmail.com>
2023-06-18 15:40:30 +0530

    Fixed Errors

--- config/socket.js
+++ config/socket.js
@@ -1,31 +1,35 @@
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 
 export default function initializeSocket(server) {
-
   const io = new Server(server, {
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
     },
   });
-  
-  
-  io.on('connection', (socket) => {
-  
+
+  let socket; 
+
+  io.on('connection', (connectionSocket) => {
+    socket = connectionSocket;
+
     socket.on('setup', (user) => {
-      socket.join(user)
-      socket.emit('connected')
-    })
-  
+      socket.join(user);
+      socket.emit('connected');
+    });
+
     socket.on('join chat', (room) => {
-      socket.join(room)
-    })
-  
+      socket.join(room);
+    });
+
     socket.on('new message', (newMessage) => {
       io.to(newMessage.conversationId).emit('message received', newMessage);
     });
-    // socket.on('disconnect', () => {
-    //   delete clients[socket.id];
-    // });
   });
+
+  return () => {
+    if (socket) {
+      socket.disconnect();
+    }
+  };
 }
